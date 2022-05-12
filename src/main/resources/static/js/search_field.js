53a553adf1e6d7c2fa71d22a021a79d80c8e6bba
defr0stt <dima.sb713@gmail.com>
2022-05-12 14:48:57 +0300

    Added price slider and visual optimisation

--- src/main/resources/static/js/search_field.js
+++ src/main/resources/static/js/search_field.js
@@ -1,24 +1,31 @@
 function requiredName() {
     const inputField = document.querySelector('input');
     let divArray = document.getElementsByClassName('alert alert-info mt-2');
+    document.getElementById('resultSet').style.display = 'none';
     // console.log(divArray);
+    let counter = 0;
     for (let i = 0; i < divArray.length; i++) {
         let temp = divArray[i].getElementsByTagName('h3');
         let spanArray = divArray[i].getElementsByTagName('span');
         if(checkCorrection(spanArray,temp[1]) && inputField.value.length != 0){
             let regExp = new RegExp('.*' + inputField.value + '.*');
             if(temp[0].innerHTML.match(regExp)){
-                if(checkCorrection(spanArray)){
+                if(checkCorrection(spanArray,temp[1])){
                     divArray[i].style.display = 'block';
+                    counter++;
                 }
             } else {
                 divArray[i].style.display = 'none';
             }
         } else if(checkCorrection(spanArray,temp[1])){
             divArray[i].style.display = 'block';
+            counter++;
         } else {
             divArray[i].style.display = 'none';
         }
+    }
+    if (counter == 0){
+        document.getElementById('resultSet').style.display='block';
     }
 }
 
@@ -52,6 +59,21 @@ function checkCorrection(componentInformation, componentPrice){
     } else {
         return false;
     }
+    if (changeSliderValue('priceMax','priceMaxSpan') - changeSliderValue('priceMin','priceMinSpan') >= 0){
+        let temp_price = '';
+        for (let i = 0; i < componentPrice.innerHTML.length; i++) {
+            if(componentPrice.innerHTML[i] == '$'){
+                break;
+            }
+            temp_price += componentPrice.innerHTML[i];
+        }
+        if(!((Number)(temp_price)>=changeSliderValue('priceMin','priceMinSpan') &&
+            (Number)(temp_price)<=changeSliderValue('priceMax','priceMaxSpan'))){
+            return false;
+        }
+    } else {
+        return false;
+    }
     if(!(checkBoxValue('checkedDesktop') && checkBoxValue('checkedLaptop'))){
         if (checkBoxValue('checkedDesktop') && componentInformation[1].innerHTML == 'Desktop'){
             return true;
@@ -71,23 +93,44 @@ function checkCorrection(componentInformation, componentPrice){
         let width = window.innerWidth;
          let element = document.getElementById('freq');
          let check = document.getElementById('type');
-        if(width <= 994){
-            if(width <= 575){
-                check.style.margin = '';
-                check.className = 'container mt-5';
-            } else {
-                check.style.margin = '-80px 0 0 45%';
-                check.className = '';
-            }
-            element.style.margin = '';
-            element.className = 'container mt-5';
-            element.style.width = '';
-        } else {
-            element.style.width = '60%';
-            check.style.margin = '-80px 0 0 45%';
+        let price = document.getElementById('price');
+        // console.log(width);
+        if(width <= 991){
+            check.style.margin = '-80px 70% 0 14%';
             check.className = '';
-            element.style.margin = '-75px 0 0 26%';
+            element.style.width = '50%';
+            price.style.width = '50%';
+            element.style.margin = '-75px 0 0 38%';
+            element.className = '';
+            price.style.margin='25px 0 0 38%';
+            price.className='';
+            if(width<=855){
+                check.style.margin = '-80px 70% 0 9%';
+            }
+            if(width <= 575){
+                element.style.margin = '';
+                element.className = 'container mt-5';
+                element.style.width = '';
+                price.style.margin = '';
+                price.className = 'container mt-5';
+                price.style.width = '';
+                check.className = 'container mt-5';
+                check.style.margin = '';
+            } else if(width<=767){
+                element.style.margin = '-75px 0 0 65%';
+                price.style.margin = '25px 0 0 65%';
+                if(width>=658){
+                    check.style.margin = '-80px 70% 0 17%';
+                }
+            }
+        } else {
+            element.style.width = '40%';
+            price.style.width = '37%';
+            check.style.margin = '-85px 0 0 48%';
+            check.className = '';
+            element.style.margin = '-75px 0 0 27%';
             element.className='';
+            price.style.margin='-75px 0 0 62%';
         }
     };
 })();
